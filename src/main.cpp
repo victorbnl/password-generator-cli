8e73409d1b5e68acc38997b0bce553bc52698101
Victor B <victor.bonnelle@protonmail.com>
2021-04-20 12:40:16 +0200

    Use arguments

--- src/main.cpp
+++ src/main.cpp
@@ -6,67 +6,44 @@
 #include "password.h"
 #include "exceptions.h"
 
-int main() {
-    std::cout << std::endl;
-
-    int length;
-    std::cout << "How long do you want your password to be? : ";
-    std::cin >> length;
-
-    std::string input;
-
-    bool lettersEnabled;
-    do {
-        std::cout << "Do you want letters? [Y/n] : ";
-        std::cin >> input;
-    } while (input != "y" && input != "n");
-
-    if (input == "y") {
-        lettersEnabled = true;
-    } else if (input == "n") {
-        lettersEnabled = false;
+int main(int argc, char* argv[]) {
+    int length = 8;
+    bool letters = true;
+    bool numbers = true;
+    bool specials = true;
+    for (int i = 0; i < argc; i++) {
+        std::string arg = std::string(argv[i]);
+        std::cout << arg << std::endl;
+        if (arg == "--help") {
+            std::cout
+                << std::endl
+                << "Usage: passgen [arguments]" << std::endl
+                << std::endl
+                << "    --help" << std::endl
+                << "        Displays this menu and exit" << std::endl
+                << std::endl
+                << "    --no-letters" << std::endl
+                << "        Disables letters" << std::endl
+                << std::endl
+                << "    --no-numbers" << std::endl
+                << "        Disables numbers" << std::endl
+                << std::endl
+                << "    --no-specials" << std::endl
+                << "        Disables special characters" << std::endl
+                << std::endl;
+            return 0;
+        } else if (arg == "--no-letters") {
+            letters = false;
+        } else if (arg == "--no-numbers") {
+            numbers = false;
+        } else if (arg == "--no-specials") {
+            specials = false;
+        }
     }
 
-    bool numbersEnabled;
-    do {
-        std::cout << "Do you want numbers? [Y/n] : ";
-        std::cin >> input;
-    } while (input != "y" && input != "n");
+    std::string password = generatePassword(length, letters, numbers, specials);
 
-    if (input == "y") {
-        numbersEnabled = true;
-    } else if (input == "n") {
-        numbersEnabled = false;
-    }
-
-    bool specialsEnabled;
-    do {
-        std::cout << "Do you want special characters? [Y/n] : ";
-        std::cin >> input;
-    } while (input != "y" && input != "n");
-
-    if (input == "y") {
-        specialsEnabled = true;
-    } else if (input == "n") {
-        specialsEnabled = false;
-    }
-
-    std::cout << std::endl;
-
-    std::string password;
-    try {
-        password = generatePassword(length, lettersEnabled, numbersEnabled, specialsEnabled);
-    } catch(const NoTypesError& e) {
-        std::cout << "So basically you want a password with no characters O.o" << std::endl;
-        std::cout << std::endl;
-        return 1;
-    } catch(const ZeroLengthPassword& e) {
-        std::cout << "On what kind of sites would you use a zero-length password 🤔" << std::endl;
-        std::cout << std::endl;
-        return 1;
-    }
-    std::cout << "Your new password is: " << password << std::endl;
-    std::cout << std::endl;
+    std::cout << "Your new password is : " << password << std::endl;
 
     return 0;
 }
